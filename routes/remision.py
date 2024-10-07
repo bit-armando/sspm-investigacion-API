97110e67fc466211117e3da8afe418fa73647ce6
Armando Rosales <refill.1509@gmail.com>
2024-10-07 12:37:59 -0600

    Correcion de api en remision con el try

--- routes/remision.py
+++ routes/remision.py
@@ -16,18 +16,20 @@ def ver_pdf(id: str):
     with open("assets/documentos/LOGO.png", "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-    data = load_json(f'remisiones/'+id+'.json')
-    data['hechos'] = salto_linea(data['hechos'])
-    data['logo'] = encoded_string
-
     ruta_template = 'assets/documentos/remision.html'
     ruta_css = 'assets/documentos/remision.css'
     try:
+        data = load_json(f'remisiones/' + id + '.json')
+        data['hechos'] = salto_linea(data['hechos'])
+        data['logo'] = encoded_string
         crea_pdf(ruta_template, data, ruta_css)
         return FileResponse('test.pdf', media_type='application/pdf')
     except:
         try:
             search_news()
+            data = load_json(f'remisiones/' + id + '.json')
+            data['hechos'] = salto_linea(data['hechos'])
+            data['logo'] = encoded_string
             crea_pdf(ruta_template, data, ruta_css)
             return FileResponse('test.pdf', media_type='application/pdf')
         except:
